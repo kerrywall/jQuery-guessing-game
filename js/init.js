842db546f421468bf8727a0f5c88a0c41640d1c7
kerrywall <kerry.wall@gmail.com>
2014-12-09 19:27:51 -0500

    js fix

--- js/init.js
+++ js/init.js
@@ -1,99 +1,101 @@
+var counter = 0;
+
 var content = [
 
   {
     "name": "Nomad's Dream",
     "answer": "polish",
-	"fullAnswer": "nail polish shade"
+  "fullAnswer": "nail polish shade"
   },
   
   {
     "name": "Meet Me On The Star Ferry",
     "answer": "polish",
-	"fullAnswer": "nail polish shade"
+  "fullAnswer": "nail polish shade"
   },
   
   {
     "name": "Dangerous Liasion",
     "answer": "polish",
-	"fullAnswer": "nail polish shade"
+  "fullAnswer": "nail polish shade"
   },
   
   {
     "name": "In My Back Pocket",
     "answer": "polish",
-	"fullAnswer": "nail polish shade"
+  "fullAnswer": "nail polish shade"
   },
   
   {
     "name": "Black Onyx",
     "answer": "polish",
-	"fullAnswer": "nail polish shade"
+  "fullAnswer": "nail polish shade"
   },
   
   {
     "name": "Lights of Emerald City",
     "answer": "polish",
-	"fullAnswer": "nail polish shade"
+  "fullAnswer": "nail polish shade"
   },
   
   {
     "name": "What Wizardry Is This?",
     "answer": "polish",
-	"fullAnswer": "nail polish shade"
+  "fullAnswer": "nail polish shade"
   },
   
   {
     "name": "Eternal Flame",
     "answer": "polish",
-	"fullAnswer": "nail polish shade"
+  "fullAnswer": "nail polish shade"
   },
   
   {
     "name": "I Feel The Earth Move",
     "answer": "polish",
-	"fullAnswer": "nail polish shade"
+  "fullAnswer": "nail polish shade"
   },
   
   {
     "name": "Garden of Eden",
     "answer": "polish",
-	"fullAnswer": "nail polish shade"
+  "fullAnswer": "nail polish shade"
   },
   
   {
     "name": "Thunder Road",
     "answer": "polish",
-	"fullAnswer": "nail polish shade"
+  "fullAnswer": "nail polish shade"
   },
   
   {
     "name": "Days of Wine and Roses",
     "answer": "polish",
-	"fullAnswer": "nail polish shade"
+  "fullAnswer": "nail polish shade"
   },
   
   {
     "name": "The Man Trap",
     "answer": "trek",
-	"fullAnswer":"Star Trek episode"
+  "fullAnswer":"Star Trek episode"
   },
   
   {
     "name": "The Naked Time",
     "answer": "trek",
-	"fullAnswer":"Star Trek episode"
+  "fullAnswer":"Star Trek episode"
   },
   
   {
     "name": "What Are Little Girls Made Of?",
     "answer": "trek",
-	"fullAnswer":"Star Trek episode"
+  "fullAnswer":"Star Trek episode"
   },
   
   {
     "name": "Tomorrow Is Yesterday",
     "answer": "trek",
-	"fullAnswer":"Star Trek episode"
+  "fullAnswer":"Star Trek episode"
   },
   
   {
@@ -104,97 +106,82 @@ var content = [
   {
     "name": "The Apple",
     "answer": "trek",
-	"fullAnswer":"Star Trek episode"
+  "fullAnswer":"Star Trek episode"
   },
   
   {
     "name": "Day Of The Dove",
     "answer": "trek",
-	"fullAnswer":"Star Trek episode"
+  "fullAnswer":"Star Trek episode"
   },
   
   {
     "name": "The Way To Eden",
     "answer": "trek",
-	"fullAnswer":"Star Trek episode"
+  "fullAnswer":"Star Trek episode"
   },
   
   {
     "name": "Friday's Child",
     "answer": "trek",
-	"fullAnswer":"Star Trek episode"
+  "fullAnswer":"Star Trek episode"
   },
   
   {
     "name": "All Our Yesterdays",
     "answer": "trek",
-	"fullAnswer":"Star Trek episode"
+  "fullAnswer":"Star Trek episode"
   },
   
   {
     "name": "Is There No Truth In Beauty?",
     "answer": "trek",
-	"fullAnswer":"Star Trek episode"
+  "fullAnswer":"Star Trek episode"
   },
   
   {
     "name": "Obsession",
     "answer": "trek",
-	"fullAnswer":"Star Trek episode"
+  "fullAnswer":"Star Trek episode"
   }
   
-];
-
-var trekApp = {};
-
-trekApp.init = function() {
-    
-    var options = content.length,
-        selection = content[Math.floor(Math.random()*options)],
-        type = selection["answer"],
-        fullAnswer = selection["fullAnswer"],
-        $name = $('#name'),
-        $generate = $('#generate'),
-        $result = $('#result'),
-        $options = $('#options');
-
-    $name.html(selection["name"]);
-    $generate.hide();
-    $result.hide();
-
-  $('.choice').on('click', function(e) {
-    var chosenAnswer = e.target.id;
-    
-    $result.show();
-    $name.hide();
-    $options.hide();
-      
-    if (chosenAnswer == type) {
-      $('#result').html("<span class='right'>Affirmative, captain!</span> It's a " + fullAnswer + ".");
-    } else {
-      $('#result').html("<span class='wrong'>Dammit, Jim!</span> It's a " + fullAnswer + ".");
-    }
-
-    $generate.show();
-    
-  });
-
-   $generate.on('click', function() {
-    
-    var selection = content[Math.floor(Math.random()*options)];
-    $name.html(selection["name"]);
-    var type = selection["answer"];
-    var fullAnswer = selection["fullAnswer"];
-    
-    $result.hide();
-    $name.show();
-    $options.show();
-    $generate.hide();
-  });
-}
-
-
+]
 $(document).ready(function() {
-  trekApp.init();
+  var selection = content[Math.floor(Math.random()*24)];
+  type = selection["answer"];
+  fullAnswer = selection["fullAnswer"];
+  $('#name').html(selection["name"]);
+  $('#generate').hide();
+  $('#result').hide();
+
 });
 
+ $('#generate').click(function() {
+  
+  var selection = content[Math.floor(Math.random()*24)];
+  $('#name').html(selection["name"]);
+  type = selection["answer"];
+  fullAnswer = selection["fullAnswer"];
+  
+  $('#result').hide();
+  $('#name').show();
+  $('#options').show();
+  $('#generate').hide();
+});
+
+$('.choice').click(function(e) {
+  var chosenAnswer = e.target.id;
+  
+  $('#result').show();
+  $('#name').hide();
+  $('#options').hide();
+    
+  if (chosenAnswer == type) {
+    $('#result').html("<span class='right'>Affirmative, captain!</span> It's a " + fullAnswer + ".");
+  } else {
+    $('#result').html("<span class='wrong'>Dammit, Jim!</span> It's a " + fullAnswer + ".");
+  }
+
+  $('#generate').show();
+  
+});
